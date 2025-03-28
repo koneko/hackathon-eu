52f322addd14c93e2ca784780fb508b4850284be
Robert <robert-letica@skole.hr>
2025-03-28 17:00:36 +0100

    authApi

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import express from "express";
-import bodyParser from "body-parser";
 import * as dbUtil from "./utils/dbUtil.js"
 
 const app = express();
 const port = process.env.port || 3000;
 
 app.use(express.static("public"));
-app.use(bodyParser.json());
+app.use(express.json());
 
 await dbUtil.connect()
 
@@ -24,11 +23,44 @@ app.get("/testing", async (req, res) => {
 	res.send("Token good!");
 });
 
-app.get("/mkAcc", async (req, res) => {
-	let usr = await dbUtil.createUser("Marko", "Markic", "marko@mail.com", "Student", ["c#", "C++"]);
-	let kita2 = await dbUtil.createSession(usr);
-	let kita = (await dbUtil.validateEmailCode(usr.usr_id, kita2.mail_code)).toString()
-	res.send(kita2.session_id);
+// {
+// 	"name": "Ime",
+// 	"lastName": "Prezime",
+// 	"mail": "mail",
+// 	"accType": "account type/ np student",
+// 	"tags": []
+// }
+
+app.post("/api/makeAccount", async (req, res) => {
+	let name = req.body.name;
+	let lastName = req.body.lastName;
+	let mail = req.body.mail;
+	let accType = req.body.accType;
+	let tags = req.body.tags;
+
+	try{
+		let usr = await dbUtil.createUser(name, lastName, mail, accType, tags);
+		if(!usr) { return res.send(400); }
+		let sesh = await dbUtil.createSession(usr);
+		if(!sesh) { return res.send(400); }
+		return res.json({"user_id": usr.usr_id});
+	} catch {
+		return res.send(400);
+	}
+});
+
+app.post("/api/verify/mailcode", async (req, res) => {
+	let mail_code = req.body.mail.mail_code;
+	let user_id = req.body.mail.user_id;
+
+	try{
+		if(await dbUtil.validateEmailCode(user_id, mail_code)){
+			let sesh = await dbUtil.getUserSession(user_id);
+			return res.json({"authorization": sesh.session_id});
+		}
+	} catch {
+		return res.send(400);
+	}
 });
 
 app.listen(port, () => {
--- utils/sessionUtil.js
+++
@@ -1,6 +0,0 @@
-// tokens/sessions
-// -> create session
-// -> validate session (1|0 -> check if exp)
-// mail code
-
-import { createUser } from "./dbUtil"
