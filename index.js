12ec17ff3bd893d4d793e6d241e17b0b229140da
koneko <67551503+koneko@users.noreply.github.com>
2025-03-28 12:25:09 +0100

    docker works for development environment

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.port || 3000;
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-	res.send("Hello, World!");
+	res.send("Hello, World");
 });
 
 app.listen(port, () => {
